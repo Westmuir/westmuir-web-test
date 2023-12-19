8ee814c13e15f98ee2e36e5e7aac1e2d55999b73
philip ross <philip@satchel.scot>
2023-12-19 11:03:02 +0000

    feat: add rsync keys and paassthru favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.addPassthroughCopy('src/favicon.ico');
   // eleventyConfig.addPassthroughCopy("src/**/*.html");
   eleventyConfig.addPassthroughCopy('src/PDF Pages');
 
