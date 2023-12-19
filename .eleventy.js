53e0da3dba406f07f23c6494e24cc6ffce108f0f
philip ross <philip@satchel.scot>
2023-12-19 14:18:56 +0000

    feat: remove table layout items

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
   // eleventyConfig.addPassthroughCopy("src/**/*.html");
   eleventyConfig.addPassthroughCopy('src/PDF Pages');
-  eleventyConfig.addPassthroughCopy('src/css/site.css');
+  eleventyConfig.addPassthroughCopy('src/css/*.css');
+
+  eleventyConfig.addWatchTarget('./src/css/');
 
   eleventyConfig.addGlobalData('permalink', () => {
     return data => `${data.page.filePathStem}.${data.page.outputFileExtension}`;
