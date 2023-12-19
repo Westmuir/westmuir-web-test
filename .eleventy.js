d9320f112b7a4aafa937cdb4dd569f6318cd7222
philip ross <philip@satchel.scot>
2023-12-19 12:27:48 +0000

    feat: footer nav using nav.njk

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
   // eleventyConfig.addPassthroughCopy("src/**/*.html");
   eleventyConfig.addPassthroughCopy('src/PDF Pages');
+  eleventyConfig.addPassthroughCopy('src/css/site.css');
 
   eleventyConfig.addGlobalData('permalink', () => {
     return data => `${data.page.filePathStem}.${data.page.outputFileExtension}`;
